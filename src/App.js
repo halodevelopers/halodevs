b1971be3d3b6ed52a5b9c2cce05712bed8a2207e
kuyesu <2019bce028@std.must.ac.ug>
2022-03-22 17:07:58 +0300

    testing workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing oif the workflow is working
         </a>
       </header>
     </div>
