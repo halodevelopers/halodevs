2caf6e7e7764dfac652cbfd939fb532de2c518be
kuyesu <2019bce028@std.must.ac.ug>
2022-03-24 16:57:53 +0300

    change every content

--- README.md
+++ README.md
@@ -1,70 +1,54 @@
-# Getting Started with Create React App
+# CODE ESI DOCS
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+[docs.codeesi.com](https://docs.codeesi.com)
 
-## Available Scripts
+## About the project
 
-In the project directory, you can run:
+This project contains all CODE ESI project documentations, events, competitions and sharing nights meetings ...
 
-### `npm start`
+![image](https://user-images.githubusercontent.com/72823374/152121606-b578a65c-0b1c-4a3f-9d91-5c928e3c37c6.png)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Used technical skills
 
-### `npm test`
+- ReactJS
+- Typescript
+- Docusaurus
+- Vercel
+- Figma
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Project Folders
 
-### `npm run build`
+### **/blog:** for competitions
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### **/blog-events:** for CODE events
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### **/docs:** for CODE training classes and projects
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+![image](https://user-images.githubusercontent.com/72823374/152121913-eb362368-e547-4cdd-96cb-658b3a351fd5.png)
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+### **/docs-sharing-night:** for CODE sharing nights
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+![image](https://user-images.githubusercontent.com/72823374/152122016-1c29600c-0f72-470d-9b77-a8fae088d358.png)
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+### **/src/pages:** for project ReactJS pages
 
-## Learn More
+### **/static/img:** for project static images
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+# CODE ESI - Official Site -
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+[codeesi.com](https://codeesi.com)
 
-### Code Splitting
+`source code: ` [CODE ESI FRONTEND](https://github.com/AhmedCoolProjects/CodeESIFrontend)
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+# CODE ESI BACKEND
 
-### Analyzing the Bundle Size
+[https://codeesiapiv3.herokuapp.com](https://codeesiapiv3.herokuapp.com)
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+`source code: ` [CODE ESI GRAPHQL API](https://github.com/AhmedCoolProjects/CodeESIGraphqlAPI)
 
-### Making a Progressive Web App
+# Created By
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+[Ahmed Bargady](https://ahmedbargdy.me)
---
+++ docusaurus.config.js
@@ -0,0 +1,232 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "HALODEVS",
+  tagline: "HALODEVS, WORKSHOPS, AND EVENTS",
+  // description: "HALODEVS ( Club Of Data Engineers ESI )",
+  url: "https://codeesi.com",
+  staticDirectories: ["static"],
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "halodeveloperss", // Usually your GitHub org/user name.
+  projectName: "halodevs", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          lastVersion: "current",
+          // Please change this to your repo.
+          editUrl: "https://github.com/halodevelopers/halodevs/tree/main",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/halodevelopers/halodevs/tree/main",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+  plugins: [
+    [
+      "@docusaurus/plugin-content-docs",
+      /** @type {import('@docusaurus/plugin-content-docs').Options} */
+      {
+        id: "docs-projects",
+        path: "docs-projects",
+        routeBasePath: "docs-projects",
+        sidebarPath: require.resolve("./sidebars.js"),
+        editUrl: "https://github.com/halodevelopers/halodevs/tree/main",
+        showLastUpdateAuthor: true,
+        showLastUpdateTime: true,
+        editCurrentVersion: true,
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      /** @type {import('@docusaurus/plugin-content-docs').Options} */
+      {
+        id: "docs-sharing-night",
+        path: "docs-sharing-night",
+        routeBasePath: "docs-sharing-night",
+        sidebarPath: require.resolve("./sidebars.js"),
+        editUrl: "https://github.com/halodevelopers/halodevs/tree/main",
+        showLastUpdateAuthor: true,
+        showLastUpdateTime: true,
+        editCurrentVersion: true,
+      },
+    ],
+
+    [
+      "@docusaurus/plugin-content-blog",
+      {
+        id: "blog-events",
+        path: "blog-events",
+        editUrl: `https://github.com/halodevelopers/halodevs/tree/main`,
+        routeBasePath: "blog-events",
+        include: ["**/*.{md,mdx}"],
+        showReadingTime: true,
+      },
+    ],
+  ],
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "HALODEVS",
+        logo: {
+          alt: "halodevs logo",
+          src: "img/halodevs.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Training",
+          },
+          {
+            to: "/docs-projects/intro",
+            docId: "intro",
+            position: "left",
+            label: "Projects",
+          },
+          {
+            to: "/blog/intro",
+            label: "Competitions",
+            position: "left",
+          },
+          {
+            to: "/docs-sharing-night/intro",
+            label: "Sharing Nights",
+            position: "left",
+            activeBaseRegex: `/docs-sharing-night/`,
+          },
+          {
+            to: "/blog-events/intro",
+            label: "Events",
+            position: "left",
+          },
+          {
+            href: "https://halodevelopers.github.io/halodevs/",
+            label: "HALODEVS",
+            position: "right",
+          },
+          {
+            type: "search",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Toturials",
+            items: [
+              {
+                label: "Training",
+                to: "/docs/intro",
+              },
+              {
+                label: "Projects",
+                to: "/docs-projects/intro",
+              },
+            ],
+          },
+          {
+            title: "Events",
+            items: [
+              {
+                label: "Competitions",
+                to: "/blog/intro",
+              },
+              {
+                label: "Sharing Nights",
+                to: "/docs-sharing-night/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Github",
+                href: "https://github.com/halodevelopers",
+              },
+              {
+                label: "Kaggle",
+                href: "https://www.kaggle.com/halodevs",
+              },
+              {
+                label: "Facebook",
+                href: "https://www.facebook.com/groups/603128047491429/",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/company/codesi19",
+              },
+              {
+                label: "YouTube",
+                href: "https://youtube.com/channel/UCStGpNYpG_2ROS6E6T-mvPg",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "codeesi.com",
+                href: "https://codeesi.com",
+              },
+              {
+                label: "esi.codeesi.com",
+                href: "https://esi.codeesi.com",
+              },
+              {
+                label: "enactus.codeesi.com",
+                href: "https://enactus.codeesi.com",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} <a href="https://ahmedbargady.me" target="_blank">JinaCoolProjects</a>. Created with love by Ahmed Bargady`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      // algolia: {
+      //   // The application ID provided by Algolia
+      //   appId: "YOUR_APP_ID",
+
+      //   // Public API key: it is safe to commit it
+      //   apiKey: "YOUR_SEARCH_API_KEY",
+
+      //   indexName: "YOUR_INDEX_NAME",
+
+      //   // Optional: see doc section below
+      //   contextualSearch: true,
+
+      //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      //   externalUrlRegex: "external\\.com|domain\\.com",
+
+      //   // Optional: Algolia search parameters
+      //   searchParameters: {},
+
+      //   //... other Algolia params
+      // },
+    }),
+};
+
+module.exports = config;
