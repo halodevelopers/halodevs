5c25eef1543a275f2616fe4d0af50a04d36734c1
kuyesu <2019bce028@std.must.ac.ug>
2022-03-24 17:26:38 +0300

    change footer settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -178,7 +178,7 @@ const config = {
               },
               {
                 label: "YouTube",
-                href: "",
+                href: "https://youtube.com/",
               },
             ],
           },
