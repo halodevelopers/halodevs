f80918a57aaecf10569112e6980362358c6ec4f0
kuyesu <2019bce028@std.must.ac.ug>
2022-03-27 12:46:57 +0000

    front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,15 +11,20 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
 
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx("hero ", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className={clsx("button button--primary button--lg button--sm ", styles.button)}
             to="/docs/intro">
             Check our projects tutorials
+          </Link>
+          <Link
+            className={clsx("button button--primary button--lg button--sm ", styles.buttonsecond)}
+            >
+            Hackathons
           </Link>
         </div>
       </div>
